76b873ac94f6be2d2f841c6d75a59c2483094b36
MatyazhV <74376954+MatyazhV@users.noreply.github.com>
2021-02-18 14:32:16 +0500

    Add files via upload

---
+++ Лаб 10/Лаб 10.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n;
+	cout << "Введите кол-во строк: ";
+	cin >> n;
+	string* a = new string[n];
+	cin.ignore();
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Введите " << i + 1 << " строку" << endl;
+		getline(cin, a[i]);
+	}
+	int j = 0;
+	while (j < n)
+	{
+		if (a[j][0] == 'a'|| a[j][0] == 'A')
+		{
+			for(int i = j;i<n - 1;i++)
+			{
+				a[i] = a[i + 1];
+			}
+			n--;
+		}
+		else
+		{
+			j++;
+		}
+	}
+	cout << "Массив после удаления: " << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << endl;
+	}
+	delete[]a;
+}
